352f33ff7ff5e8241430bfa407955517fa5cf124
RejectorYou <fathurarrasyid24@gmail.com>
2024-08-21 20:46:25 +0700

    Python Variable -rev4

--- basic/Variable.py
+++ basic/Variable.py
@@ -1,3 +1,6 @@
+import os
+os.system("cls")
+
 My_Name = "Fathurrahman"
 place = "Bandung"   
 buah = ["Apel","Mangga","Anggur","Pir"]
@@ -7,4 +10,4 @@ print("Nama Saya    : ",My_Name)
 print("Asal Saya    : ",place)
 print("Buah Favorit : ",b1,b2,b3,b4)
 
-print("ALL DATA VARIABLE\n"+My_Name+place+buah+"\n")
+print("ALL DATA VARIABLE\n"+My_Name+place+b1+b2+b3+b4+"\n")
