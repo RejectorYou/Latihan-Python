391bd1f23ee71c8fa53f843ea200dd36c755e44c
RejectorYou <fathurarrasyid24@gmail.com>
2024-08-21 21:11:26 +0700

    Python Numberic

---
+++ basic/numeric.py
@@ -0,0 +1,21 @@
+import os
+os.system('cls')
+x=5
+y=2
+z=16
+
+j=x+y+z
+
+print(x," + ",y," + ",z," = ",j)
+
+x=float(x)
+y=complex(y)
+z=int(z)
+
+print(x,y,z)
+print(type(x))
+print(type(y))
+print(type(z))
+
+import random
+print(random.randrange(000000000,99999999999))
