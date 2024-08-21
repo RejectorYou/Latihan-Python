1b09f091093870bdb96f999538f7a48288aa9a5c
RejectorYou <fathurarrasyid24@gmail.com>
2024-08-21 21:04:55 +0700

    Python Data Type

---
+++ basic/data_type.py
@@ -0,0 +1,33 @@
+import os
+os.system('cls')
+
+x="Hello World" 
+print(x,type(x))
+x=10 
+print(x,type(x))
+x=2.5
+print(x,type(x))
+x=1j 
+print(x,type(x))
+x=["apel","mangga","jambu"]
+print(x,type(x))
+x=("rumah","mobil","keluarga")
+print(x,type(x))
+x=range(6)
+print(x,type(x))
+x={"Nama":"Fathur","Alamat":"Bandung"}
+print(x,type(x))
+x={"Harta","Tahta","Wanita"}
+print(x,type(x))
+x=frozenset({"SD","SMP","SMK"})
+print(x,type(x))
+x=True
+print(x,type(x))
+x=b"Hello"
+print(x,type(x))
+x=bytearray(7)
+print(x,type(x))
+x=memoryview(bytes(5))
+print(x,type(x))
+x=None
+print(x,type(x))
