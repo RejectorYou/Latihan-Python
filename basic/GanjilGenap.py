33417f4a58db632c17d7cefecd94ef235958797a
RejectorYou <fathurarrasyid24@gmail.com>
2024-08-22 20:27:52 +0700

    update ganjil genap

--- basic/GanjilGenap.py
+++ basic/GanjilGenap.py
@@ -1,6 +1,44 @@
-i = 8
-gg = i%2
-if (gg==1):
-    print("Variabel i=",i," <- Adalah Bilangan Ganjil")
-else:
-    print("Variabel i=",i," <- Adalah Genap")
+import os
+
+loop=True
+
+def line():
+    j=""
+    i=0
+    while(i<=100):
+        j+="="
+        i+=1
+
+    return j
+
+garis = line()
+
+
+os.system('cls')
+
+while loop == True:
+    while True:
+        try:
+            print(garis)
+            print("Aplikasi Menentukan Bilangan Ganjil atau Genap")
+            print(garis)
+            i=int(input("Masukan bilangan : "))
+            break
+        except:
+            os.system('cls')
+            print("Hanya masukan bilangan saja !!!")
+    gg = i%2
+    print(garis)
+    if (gg==1):
+        print("Variabel i=",i,"<- Adalah Bilangan Ganjil")
+    else:
+        print("Variabel i=",i,"<- Adalah Bilangan Genap")
+    print(garis)
+    loop = input("Try Agan ? (y/n)")
+    loop = loop.upper()
+    if loop=="Y":
+        loop=True
+        os.system('cls')
+    else:
+        break
+
